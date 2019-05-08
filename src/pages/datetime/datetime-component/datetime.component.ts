632bae12a9e6d7d77673e0b2afe7dae68f04ac00
pjaudiomv <pjaudiomv@gmail.com>
2019-05-08 12:53:57 -0400

    add cleantime in year, months, weeks, days

--- src/pages/datetime/datetime-component/datetime.component.ts
+++ src/pages/datetime/datetime-component/datetime.component.ts
@@ -15,6 +15,8 @@ export class DatetimeComponent {
   cleanTimeInMonths       : any;
   cleanTimeInYears        : any;
   cleanTimeHumanize       : any;
+  cleanTimeDateDiff       : any;
+  intervals               : any;
 
   constructor( private storage     : Storage  ) {
     this.cleanDate = moment().startOf('day').format();
@@ -33,14 +35,25 @@ export class DatetimeComponent {
 
   getCleanTime(){
     this.momentCleanDate = moment(this.cleanDate).startOf('day');
-
     this.cleanTimeInDays   = moment().diff(this.momentCleanDate, 'days');
     this.cleanTimeInWeeks  = moment().diff(this.momentCleanDate, 'weeks');
     this.cleanTimeInMonths = moment().diff(this.momentCleanDate, 'months');
     this.cleanTimeInYears  = moment().diff(this.momentCleanDate, 'years');
-
+    this.cleanTimeDateDiff = this.getCleanTimeDateDiff();
     this.storage.set('cleanDate', this.cleanDate);
   }
 
+  getCleanTimeDateDiff() {
+    var b = this.momentCleanDate,
+        a = moment(),
+        out = [];
+        this.intervals = ['years','months','weeks','days'];
+    for(var i=0; i < this.intervals.length; i++){
+      var diff = a.diff(b, this.intervals[i]);
+      b.add(diff, this.intervals[i]);
+      out.push(diff + ' ' + this.intervals[i]);
+    }
+    return out.join(', ');
+  };
 
 }
